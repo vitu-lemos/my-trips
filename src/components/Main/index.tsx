dbd250a5c639630357c4cf8446926a803059d8e6
Vitor Lemos <vitor.lemos@br.experian.com>
2021-09-19 04:09:01 -0300

    Initial setup

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,2 @@
-import * as S from './styles'
-
-const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
-}) => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e ao lado React Avançado"
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Ilustration
-      src="/img/hero-illustration.svg"
-      alt="Pessoa em frente ao computador programando"
-    />
-  </S.Wrapper>
-)
+const Main = ({ title = 'React Avançado' }) => <h1>{title}</h1>
 export default Main
