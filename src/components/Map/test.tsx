bc94b2bc5074b856820d59995e699733f23d369e
Vitor Lemos <lemos.dev02@gmail.com>
2021-11-04 02:45:53 -0300

    Fix map test

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import { Places } from 'models/places'
 import Map from './Map'
 
 describe('<Map/>', () => {
@@ -12,24 +13,24 @@ describe('<Map/>', () => {
     ).toBeInTheDocument
   })
   it('should render with marker in correct location', () => {
-    const places = [
+    const places: Places = [
       {
-        id: '1',
         name: 'Vitória da Conquista',
         slug: 'vitoria-da-conquista',
         location: {
           latitude: -14.8648,
           longitude: -40.8369
-        }
+        },
+        gallery: []
       },
       {
-        id: '2',
         name: 'Penísula de Maraú',
         slug: 'marau',
         location: {
           latitude: -14.1154,
           longitude: -38.9933
-        }
+        },
+        gallery: []
       }
     ]
 
