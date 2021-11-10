cdbbfc682e395d8eafd4824fff406c706edc3c27
Vitor Lemos <lemos.dev02@gmail.com>
2021-11-09 23:26:13 -0300

    Add place page template

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,10 +1,50 @@
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+
 import { Place } from 'models/places'
+
+import LinkWrapper from 'components/LinkWrapper'
+import LoaderContainer from 'components/LoaderContainer'
+
+import * as S from './styles'
 
 interface Props {
   place: Place
 }
 const PlacesTemplate = ({ place }: Props) => {
-  return <h1>{place.name}</h1>
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <LoaderContainer loading />
+  }
+  return (
+    <>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          />
+          <S.Gallery>
+            {place.gallery.map((item) => (
+              <Image
+                height={750}
+                width={1000}
+                key={item.id}
+                src={item.url}
+                alt={place.name}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
 }
 
 export default PlacesTemplate
--- src/templates/Places/styles.ts
+++ src/templates/Places/styles.ts
@@ -1,3 +1,43 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Wrapper = styled.div`
+  max-width: var(--container);
+  margin: auto;
+`
+export const Container = styled.div`
+  padding: var(--large);
+`
+
+export const Heading = styled.h1`
+  font-size: var(--medium);
+  margin-bottom: var(--medium);
+`
+
+export const Body = styled.div`
+  margin-bottom: var(--small);
+`
+
+export const Gallery = styled.div`
+  display: grid;
+  grid-gap: var(--medium);
+  img {
+    background: #f6f7f8;
+    background-image: linear-gradient(
+      to right,
+      #f6f7f8 0%,
+      #edeef1 20%,
+      #f6f7f8 40%,
+      #f6f7f8 100%
+    );
+    background-size: 80rem 14rem;
+    animation: placeholderShimmer 1s linear infinite forwards;
+    @keyframes placeholderShimmer {
+      0% {
+        background-position: -40rem 0;
+      }
+      100% {
+        background-position: 40rem 0;
+      }
+    }
+  }
+`
