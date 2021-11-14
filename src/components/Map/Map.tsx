b035283db17f9564e92de84f7ca5a8308fe3989b
Vitor Lemos <lemos.dev02@gmail.com>
2021-11-14 01:47:09 -0300

    fix map initial coords

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -14,15 +14,19 @@ const Map = ({ places }: Props) => {
   const openPlace = (placeSlug: string) => {
     router.push(`place/${placeSlug}`)
   }
+  const initialCoord = {
+    lat: -14.2400732,
+    lng: -53.1805017
+  }
 
   return (
     <S.MapWrapper>
       <MapContainer
-        center={[51.505, -0.09]}
         zoom={3}
         scrollWheelZoom={true}
         style={{ height: '100%', width: '100%' }}
         minZoom={3}
+        center={[initialCoord.lat, initialCoord.lng]}
         maxBounds={[
           [-180, 180],
           [180, -180]
